45b1dba39fca982dc267908d2d7eafa746f1b525
Jibran Tariq <k143408@nu.edu.pk>
2020-05-08 04:10:27 +0400

    Project Structure.

---
+++ src/main/java/com/code/exercise/matches/FilteringMatchesApplication.java
@@ -0,0 +1,13 @@
+package com.code.exercise.matches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilteringMatchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilteringMatchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/exercise/matches/FilteringMatchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.exercise.matches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilteringMatchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
